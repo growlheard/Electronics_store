1c6eab60f0280241f7aa9f7fd6937d8ba78a5a2c
Growlheard <growlheard@gmail.com>
2023-03-03 12:57:17 +0300

    add methods repr and str

--- electronics_store/main.py
+++ electronics_store/main.py
@@ -17,6 +17,13 @@ class Item:
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return self.__name
+
     @property
     def name(self):
         """
